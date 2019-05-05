8a54e977e95ac9a561eebc60e9ecf66d3b459dfd
fritsonagung <agungfritson@gmail.com>
2019-05-05 16:58:07 +0700

    Semua Activity dan Fragment Berfungsi, Kecuali Memainkan Musik Dari RecyclerView

---
+++ app/src/main/java/com/example/fritsonapps/presenter/MusicFavoritePresenter.java
@@ -0,0 +1,12 @@
+package com.example.fritsonapps.presenter;
+
+/*
+ Nama : Fritson Agung Julians Ayomi
+ NIM  : 10116076
+ Kelas: AKB-2
+ Tanggal Pengerjaan : 03 Mei 2019
+ */
+
+public interface MusicFavoritePresenter {
+    void load();
+}
---
+++ app/src/main/java/com/example/fritsonapps/presenter/MusicFavoritePresenterImpl.java
@@ -0,0 +1,48 @@
+package com.example.fritsonapps.presenter;
+
+/*
+ Nama : Fritson Agung Julians Ayomi
+ NIM  : 10116076
+ Kelas: AKB-2
+ Tanggal Pengerjaan : 03 Mei 2019
+ */
+
+import com.example.fritsonapps.R;
+import com.example.fritsonapps.model.MusicFavoriteModel;
+import com.example.fritsonapps.view.MusicFavoriteView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicFavoritePresenterImpl implements MusicFavoritePresenter {
+
+    private MusicFavoriteView musicFavoriteView;
+
+    private List<MusicFavoriteModel> songs = new ArrayList<>();
+
+    public MusicFavoritePresenterImpl(MusicFavoriteView musicFavoriteView) {
+        this.musicFavoriteView = musicFavoriteView;
+        setData();
+    }
+
+    private void setData() {
+        MusicFavoriteModel song = new MusicFavoriteModel();
+        song.setSong(R.raw.music_1_train_heysoulsister);
+        song.setArtwork(R.drawable.song_1);
+        song.setSongTitle("Hey, Soul Sister - Train");
+        song.setSongGenre("Pop rock, Folk rock");
+        songs.add(song);
+
+        MusicFavoriteModel song1 = new MusicFavoriteModel();
+        song1.setSong(R.raw.music_2_oor_clockstrikes);
+        song1.setArtwork(R.drawable.song_2);
+        song1.setSongTitle("Clock Strike - One Ok Rock");
+        song1.setSongGenre("Alternative rock, Power pop, Pop punk");
+        songs.add(song1);
+    }
+
+    @Override
+    public void load() {
+        musicFavoriteView.onLoad(songs);
+    }
+}
